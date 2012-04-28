13d675c5e5f73ef0baeb350d8f4a06e5004b2fcd
Anton Rogov <antonrogov@me.com>
2012-04-28 16:16:33 +0400

    using port from env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = process.argv[2] || 3000;
+    port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
   var uri = url.parse(request.url).pathname,
